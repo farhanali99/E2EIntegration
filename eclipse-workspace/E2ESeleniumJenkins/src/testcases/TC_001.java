8470eab47f2f917d9e36e7f21c3f153166c2159f
Farhan.Ali <Farhan.Ali@JLT00909L.emaratech.ae>
2017-07-25 18:37:00 +0400

    dsfdsfsfCreated Automation Project

---
+++ eclipse-workspace/E2ESeleniumJenkins/src/testcases/TC_001.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+	public void testcase1() {
+	
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://hosting.invers.com/aimex/");
+		driver.findElementById("ctl00_cph_LoginView1_LoginControl_Login1_UserName").sendKeys("Hello");
+		driver.findElementById("ctl00_cph_LoginView1_LoginControl_Login1_Password").sendKeys("Hellop");
+		
+	}
+
+}
---
+++ eclipse-workspace/loadtesting/src/test/java/loadtesting/WebDriverTest2.java
@@ -0,0 +1,20 @@
+
+package loadtesting;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WebDriverTest2 {
+	
+	@Test
+	public void testcase1() {
+	
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://hosting.invers.com/aimex/");
+		driver.findElementById("ctl00_cph_LoginView1_LoginControl_Login1_UserName").sendKeys("Hello");
+		driver.findElementById("ctl00_cph_LoginView1_LoginControl_Login1_Password").sendKeys("Hellop");
+		
+	}
+
+}
